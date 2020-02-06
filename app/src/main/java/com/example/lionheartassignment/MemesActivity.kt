a6b75af6bca54bde897402777442c64cb6a0aac9
Ashaneen <andrzej.bogacz@mail.com>
2020-02-06 17:02:59 +0000

    Add Activities and Fragments
    Specific commit details:
    - Modify MainActivity into MemesActivity storing fragments
    - Add TopMemesFragment and CreateMemesFragment
    - Modify style and colors for main Theme

---
+++ app/src/main/java/com/example/lionheartassignment/MemesActivity.kt
@@ -0,0 +1,49 @@
+package com.example.lionheartassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_memes.*
+
+class MemesActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memes)
+
+
+
+        navController = findNavController(R.id.garden_nav_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawer_layout)
+
+        // Set up ActionBar
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Set up navigation menu
+        navigation_view.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
